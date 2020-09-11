e5627d405c8406202306cdafb122b8dc8e272c8e
oaflindberg <45793849+oaflindberg@users.noreply.github.com>
2020-09-11 18:28:25 +0200

    Update: - Made Global.js Global.tsx - Added Gundla Gårdscafe | to title

--- www/components/Layout/Layout.tsx
+++ www/components/Layout/Layout.tsx
@@ -1,13 +1,15 @@
 import Head from 'next/head';
+import GlobalStyle from 'styles/Global'
 
 const Layout = ({ children, content }) => {
   return (
     <>
       <Head>
-        <title>{content[0].pageTitle}</title>
+        <title>{`Gundla Gårdscafe | ${content[0].pageTitle}`}</title>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
+      <GlobalStyle />
 
       <div style={{ display: 'flex', flexDirection: 'column' }}>{children}</div>
 
