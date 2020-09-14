e60c790b80bee4f5edc6f7bfcbf11d0c296908ec
oaflindberg <45793849+oaflindberg@users.noreply.github.com>
2020-09-14 12:15:45 +0200

    Added some basic styling to test if it works

--- www/components/Card/Card.tsx
+++ www/components/Card/Card.tsx
@@ -1,9 +1,15 @@
+import { StyledCard, Date } from './style'
+
 export default function Card({ title, date, description }) {
   return (
-    <div>
+    <StyledCard>
+      <div>
       <h1>{title}</h1>
       <p>{description}</p>
+      </div>
+      <Date>
       <h3>{date}</h3>
-    </div>
+      </Date>
+    </StyledCard>
   )
 }
---
+++ www/components/Card/style.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled'
+
+export const StyledCard = styled.div`
+  height: min-content;
+  width: max-content;
+  padding: 20px;
+  display: flex;
+`
+
+export const Date = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  width: max-content
+`
