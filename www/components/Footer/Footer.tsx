90110b88f0cdb356a80891d4726e6bebb29c8d95
oaflindberg <45793849+oaflindberg@users.noreply.github.com>
2020-09-14 21:36:30 +0200

    Footer is now working properly

--- www/components/Footer/Footer.tsx
+++ www/components/Footer/Footer.tsx
@@ -1,12 +1,13 @@
+import Link from 'next/link'
 import client from 'client/api'
 import urlFor from 'utils/urlBuilder'
 import { useState, useEffect } from 'react'
 
 export default function Footer() {
-  const [data, setData] = useState('')
+  const [data, setData] = useState([])
 
   useEffect(() => {
-    let fetch = client.fetch(`*[_type == 'footer']`).then((res) => {
+    client.fetch(`*[_type == 'footer']`).then((res) => {
       setData(res)
     })
   }, [])
@@ -15,8 +16,21 @@ export default function Footer() {
 
   return (
     <footer>
-      <p>{data[0].mail}</p>
-      <p>{data[0].telefon}</p>
+      {data.length > 0 && (
+        <>
+          <button type="button">{data[0].find_us}</button>
+          <Link href="/gallery">
+            <a>
+              <button type="button">{data[0].gallery}</button>
+            </a>
+          </Link>
+          <a href={data[0].facebook_url}>Facebook</a>
+          <a href={data[0].instagram_url}>Instagram</a>
+          <a href={data[0].twitter_url}>Twitter</a>
+          <a href={`mailto:${data[0].mail}`}>{data[0].mail}</a>
+          <a href="tel:+46708840717">{data[0].telefon}</a>
+        </>
+      )}
     </footer>
   )
 }
