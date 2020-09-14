91c6f2c060be04e6906e6b57658355617b3a3e76
oaflindberg <45793849+oaflindberg@users.noreply.github.com>
2020-09-14 10:16:59 +0200

    Renamed files

--- sanity-studio/schemas/schema.js
+++ sanity-studio/schemas/schema.js
@@ -1,16 +1,15 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from 'part:@sanity/base/schema-creator';
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 // We import object and document schemas
-import blockContent from './blockContent'
-import event from "./event"
-import menu from "./menu"
-import homepage from "./homepage";
-import catering from "./catering";
-
+import blockContent from './blockContent';
+import event from './event';
+import menu from './menu';
+import homepage from './homepage';
+import catering from './catering';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -27,6 +26,6 @@ export default createSchema({
     catering,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
-    blockContent
-  ])
-})
+    blockContent,
+  ]),
+});
