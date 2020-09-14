13ffa14c72eba411f093b78cd47e77b3b90c61e8
oaflindberg <45793849+oaflindberg@users.noreply.github.com>
2020-09-14 12:03:06 +0200

    Added date-fns to format date properly

--- www/pages/events.tsx
+++ www/pages/events.tsx
@@ -1,13 +1,14 @@
 import Layout from 'components/Layout/Layout'
 import client from 'client/api'
 import Card from 'components/Card/Card'
+import {format} from 'date-fns'
 
 export default function EventPage( { eventData }) {
   return (
     <Layout content ="">
       {eventData.map((event, i) => {
         return (
-          <Card key={i} title={event.title} date={event.date} description={event.body} />
+          <Card key={i} title={event.title} date={format(new Date(event.date), 'd LLLL')} description={event.body} />
         )
       })}
     </Layout>
