9f68497007a242d350423cc9c3d1ee3d909d3224
Thomas Sönnerstam <50919150+ThomasSonnerstam@users.noreply.github.com>
2020-09-21 16:40:59 +0200

    Added more styling

--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -4,9 +4,19 @@ import Section from 'components/Section/Section'
 import urlFor from 'utils/urlBuilder'
 import BlockContent from '@sanity/block-content-to-react'
 import { Paper, Btn } from 'styles/Startpage'
+import { useMediaQuery } from 'react-responsive'
 
 export default function Home({ homepageData }) {
   const home = homepageData[0]
+
+  const isDevice = {
+    mobile: useMediaQuery({
+      query: '(max-width: 767px)',
+    }),
+    desktop: useMediaQuery({
+      query: '(min-width: 768px)',
+    }),
+  }
 
   return (
     <Layout pageTitle="Gundla Gårdscafé | Startsida">
@@ -14,6 +24,9 @@ export default function Home({ homepageData }) {
         style={{
           backgroundImage: `url(${urlFor(home.heroImage).toString()})`,
           margin: 0,
+          height: isDevice.desktop ? '65vh' : '40vh',
+          padding: isDevice.desktop && '0 15vw',
+          justifyContent: 'center',
         }}
       >
         <h1 style={{ color: 'white' }}>{home.titleOne}</h1>
@@ -21,15 +34,29 @@ export default function Home({ homepageData }) {
         <Btn type="button">{home.catering_button}</Btn>
       </Section>
 
-      <Section>
-        <h1 style={{ marginBottom: '5px' }}>{home.titleTwo}</h1>
-        <BlockContent blocks={home.descriptionTwo} />
-      </Section>
-      <img
-        style={{ height: '30vh', objectFit: 'cover' }}
-        src={urlFor(home.imageTwo).toString()}
-      />
-      <Btn type="button">{home.calendar_button}</Btn>
+      <div
+        style={{
+          marginTop: isDevice.mobile ? '5vh' : '10vh',
+          display: isDevice.desktop && 'flex',
+        }}
+      >
+        <Section style={{ width: isDevice.desktop && '35vw' }}>
+          <h1 style={{ marginBottom: '5px', marginTop: '0' }}>
+            {home.titleTwo}
+          </h1>
+          <BlockContent blocks={home.descriptionTwo} />
+          <Btn type="button">{home.calendar_button}</Btn>
+        </Section>
+        <img
+          style={{
+            height: '50vh',
+            width: isDevice.mobile ? '100vw' : '35vw',
+            objectFit: 'cover',
+          }}
+          src={urlFor(home.imageTwo).toString()}
+        />
+      </div>
+
       <Paper>
         <Section>
           <h1>{home.titleThree}</h1>
--- www/styles/Startpage.tsx
+++ www/styles/Startpage.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled'
+import BlockContent from '@sanity/block-content-to-react'
 
 export const Paper = styled.div`
   background: url('/images/pieceofpaper.png');
@@ -15,12 +16,13 @@ export const Btn = styled.button`
   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.08);
   color: #fbf3e0;
   font-size: 1.5rem;
-  margin: 50px auto;
+  margin: 50px 0;
   margin-bottom: 20px;
-  padding: 10px 50px;
-  width: 70%;
+  width: 50vw;
   cursor: pointer;
+  height: 5vh;
 
   @media screen and (min-width: 768px) {
+    width: 20vw;
   }
 `
